96f2da3e13ac7ec87f4063a9af03522f95cee7f5
Davin <davinrim10@gmail.com>
2023-10-05 17:17:39 +0900

    add price form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/price-form.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Pencil } from 'lucide-react';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { Course } from '@prisma/client';
+import formatPrice from '@/lib/formatprice';
+
+interface PriceFormProps {
+  initialData: Course;
+  courseId: string;
+}
+
+const formSchema = z.object({
+  price: z.coerce.number(),
+});
+
+const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((prev) => !prev);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      price: initialData?.price || undefined,
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success('Course updated');
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error('Something went wrong');
+    }
+  };
+
+  return (
+    <div className='bg-slate-100 border rounded-md p-4 mt-6'>
+      <div className='flex items-center justify-between font-medium'>
+        Course Price
+        <Button variant='ghost' onClick={toggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className='h-4 w-4 mr-2' />
+              Edit
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p
+          className={cn(
+            'text-sm mt-2',
+            !initialData.price && 'italic text-slate-500'
+          )}
+        >
+          {initialData.price ? formatPrice(initialData.price) : 'No price'}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='mt-2'>
+            <FormField
+              control={form.control}
+              name='price'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type='number'
+                      step='0.01'
+                      disabled={isSubmitting}
+                      placeholder='Set a price for your course'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='mt-4'>
+              <Button type='submit' disabled={!isValid || isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default PriceForm;
