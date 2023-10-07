9692d40e526d703d6f9b69398127baca9dee5b84
Davin <davinrim10@gmail.com>
2023-10-07 15:16:41 +0900

    add chapter description form using react-quill

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-description-form.tsx
@@ -0,0 +1,125 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { Pencil } from 'lucide-react';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { Textarea } from '@/components/ui/textarea';
+import { Chapter, Course } from '@prisma/client';
+import { Editor } from '@/components/editor';
+import { Preview } from '@/components/preview';
+
+interface ChapterDescriptionFormProps {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+}
+
+const formSchema = z.object({
+  description: z.string().min(1),
+});
+
+const ChapterDescriptionForm = ({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterDescriptionFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((prev) => !prev);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: initialData?.description || '',
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${chapterId}`,
+        values
+      );
+      toast.success('Chapter updated');
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error('Something went wrong');
+    }
+  };
+
+  return (
+    <div className='bg-slate-100 border rounded-md p-4 mt-6'>
+      <div className='flex items-center justify-between font-medium'>
+        Chapter Description
+        <Button variant='ghost' onClick={toggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className='h-4 w-4 mr-2' />
+              Edit
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <div
+          className={cn(
+            'text-sm mt-2',
+            !initialData.description && 'italic text-slate-500'
+          )}
+        >
+          {!initialData.description && 'No description'}
+          {initialData.description && (
+            <Preview value={initialData.description} />
+          )}
+        </div>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='mt-2'>
+            <FormField
+              control={form.control}
+              name='description'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Editor {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='mt-4'>
+              <Button type='submit' disabled={!isValid || isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default ChapterDescriptionForm;
