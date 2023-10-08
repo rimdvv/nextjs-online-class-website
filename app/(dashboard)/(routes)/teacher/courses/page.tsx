1044c5b3ca85ecfacf4da0f38b1f0a0d7e3bd105
Davin <davinrim10@gmail.com>
2023-10-08 17:50:10 +0900

    add course data table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,32 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { DataTable } from './_components/data-table';
+import { columns } from './_components/columns';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { db } from '@/lib/db';
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
   return (
     <div className='p-6'>
-      <Link href='/teacher/create'>
-        <Button>Create New Course</Button>
-      </Link>
+      <div className='container mx-auto py-10'>
+        <DataTable columns={columns} data={courses} />
+      </div>
     </div>
   );
 };
